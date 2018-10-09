809fd2b0bc44da10e5578590425f47934111ebc8
Seth Butler <seth.butler@ibm.com>
2018-10-09 16:17:58 -0400

    freestanding-rust-binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// MacOS
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
+
+// LINUX
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
