49f3fac622c0e359a3a0a9d3153c965c7ac09512
Seth Butler <seth.butler@ibm.com>
2018-10-10 19:08:37 -0400

    Unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![no_std]
-#![no_main]
+// only enable no_main macro in non-test mode
+// which prevents a main from being injected since this is a kernel
+#![cfg_attr(not(test), no_main)]
+// silence certain warnings when testing is being performed
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
 #[macro_use]
 extern crate lazy_static;
@@ -7,12 +11,20 @@ extern crate bootloader_precompiled;
 extern crate volatile;
 extern crate spin;
 
+// Unit tests run on host machine, therefore std lib available
+#[cfg(test)]
+extern crate std;
+#[cfg(test)]
+extern crate array_init;
+
 #[macro_use]        // says to also import macros from module
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 
+// The function expected in bootloader assembly for the start of the program
+#[cfg(not(test))]
 #[no_mangle]        // ensures function name is not mangled for usage by bootloader
 pub extern "C" fn _start() -> ! {
     println!("Helo World{}", "!");
@@ -20,6 +32,7 @@ pub extern "C" fn _start() -> ! {
 }
 
 // Defines the method to use in case of a panic
+#[cfg(not(test))]       // only compile when test flag is not set
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
