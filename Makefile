3c5369e698b0e80182bc00d8f833aac06869e63c
Seth Butler <seth.butler@ibm.com>
2018-10-10 21:44:20 -0400

    CPU-Exceptions

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
+#Break qemu with "ctrl+a + x"
+
 BOOTBIN=./target/x86_64-rust_os/debug/bootimage-rust_os.bin
 
 bootbuild:
 	bootimage build
 
 bootrun:
-	bootimage run
+	bootimage run -- -serial mon:stdio -device isa-debug-exit,iobase=0xf4,iosize=0x04
 
 bootbr:
+	bootimage build && bootimage run -- -serial mon:stdio -device isa-debug-exit,iobase=0xf4,iosize=0x04
+
+bootbn:	
 	bootimage build && bootimage run -- -serial mon:stdio -device isa-debug-exit,iobase=0xf4,iosize=0x04 -display none
 
 boottest:
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,16 @@
 #![cfg_attr(not(test), no_main)]
 // silence certain warnings when testing is being performed
 #![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
+#![feature(abi_x86_interrupt)]  // enable usage of unstable x86-interrupt calling convention
 
 #[macro_use]
 extern crate rust_os;
-
+#[macro_use]
+extern crate lazy_static;
+extern crate x86_64;
 
 use core::panic::PanicInfo;
-
+use x86_64::structures::idt::{InterruptDescriptorTable, ExceptionStackFrame };
 
 // The function expected in linker for the start of the program
 #[cfg(not(test))]
@@ -18,6 +21,13 @@ use core::panic::PanicInfo;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     serial_println!("Hello Host{}", "!");
+
+    init_idt();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::int3();
+
+    println!("It did not crash!");
 
     loop {}
 }
@@ -30,3 +40,23 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+// Static IDT for CPU to reference during exceptions
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
+
+// Initialize the CPUs IDT
+pub fn init_idt() {
+    IDT.load();
+}
+
+/// Handler for breakpoint exception
+extern "x86-interrupt" fn breakpoint_handler(
+    stack_frame: &mut ExceptionStackFrame
+) {
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
